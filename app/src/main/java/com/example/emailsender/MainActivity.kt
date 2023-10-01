8f7c8a9f16319f7b85934faf745e5b0e7809f0c0
allexeu <oleksahorch@gmail.com>
2023-10-01 19:57:21 +0300

    design finished, continue working on MainActivity.kt

--- app/src/main/java/com/example/emailsender/MainActivity.kt
+++ app/src/main/java/com/example/emailsender/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.emailsender
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.emailsender.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,19 +12,27 @@ class MainActivity : AppCompatActivity() {
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.sendButton.setOnClickListener {
+            val emailValue = binding.emailInput.text.toString()
+            val subjectValue = binding.subjectInput.text.toString()
+            val textValue = binding.textInput.text.toString()
+
+            if (emailValue.isEmpty() || subjectValue.isEmpty() || textValue.isEmpty()) {
+                Toast.makeText(this, "One of the input field is empty!", Toast.LENGTH_SHORT).show()
+            } else {
+                buildIntent(emailValue, subjectValue, textValue)
+            }
+        }
     }
 
-    fun parseInput() {
-
-    }
-
-    fun buildIntent() {
+    fun buildIntent(email: String, subject: String, text: String) {
         val sendIntent = Intent(Intent.ACTION_SEND)
         sendIntent.type = "text/plain"
 
-        sendIntent.putExtra(Intent.EXTRA_EMAIL, "")
-        sendIntent.putExtra(Intent.EXTRA_SUBJECT, "")
-        sendIntent.putExtra(Intent.EXTRA_TEXT, "")
+        sendIntent.putExtra(Intent.EXTRA_EMAIL, email)
+        sendIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        sendIntent.putExtra(Intent.EXTRA_TEXT, text)
 
         this.startActivity(sendIntent)
     }
