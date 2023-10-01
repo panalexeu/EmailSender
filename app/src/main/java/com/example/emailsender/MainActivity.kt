536aa2d3f2fa9d0555bfc9495ddc9fb4ffebea5f
allexeu <oleksahorch@gmail.com>
2023-10-01 19:57:53 +0300

    finished working on the app

--- app/src/main/java/com/example/emailsender/MainActivity.kt
+++ app/src/main/java/com/example/emailsender/MainActivity.kt
@@ -19,14 +19,14 @@ class MainActivity : AppCompatActivity() {
             val textValue = binding.textInput.text.toString()
 
             if (emailValue.isEmpty() || subjectValue.isEmpty() || textValue.isEmpty()) {
-                Toast.makeText(this, "One of the input field is empty!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "One of the input fields is empty!", Toast.LENGTH_SHORT).show()
             } else {
                 buildIntent(emailValue, subjectValue, textValue)
             }
         }
     }
 
-    fun buildIntent(email: String, subject: String, text: String) {
+    private fun buildIntent(email: String, subject: String, text: String) {
         val sendIntent = Intent(Intent.ACTION_SEND)
         sendIntent.type = "text/plain"
 
